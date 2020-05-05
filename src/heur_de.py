Fixed bug in computation of R

src/heur_de.py
@@ -8,10 +8,10 @@ class DifferentialEvolution(Heuristic):
         Heuristic.__init__(self, of, maxeval)
         assert N >= 4, 'N should be at least equal to 4'
         self.N = N
+        self.n = np.size(of.a)
         self.CR = CR
         assert 0 <= F <= 2, 'F should be from [0; 2]'
         self.F = F
-        #self.mutation = mutation
 
     def search(self):
         try:
@@ -30,7 +30,7 @@ class DifferentialEvolution(Heuristic):
                     x = pop_X[i]
                     agents = np.random.choice(np.delete(np.arange(self.N), i), 3, replace=False)  # selected 3 agents
                     a, b, c = pop_X[agents[0]], pop_X[agents[1]], pop_X[agents[2]]
-                    R = np.random.randint(low=0, high=self.N)
+                    R = np.random.randint(low=0, high=self.n)
                     y = [a[j] + self.F*(b[j]-c[j]) if np.random.rand() < self.CR or j == R else x[j] for j in range(n)]
                     f_y = self.evaluate(y)
                     if f_y < pop_f[i]: