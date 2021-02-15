38dfab9d63024d4bf89483bc2736f1e4b857e2ea
Matěj Rzehulka <matej.rzehulka@fykos.cz>
2021-02-15 23:30:53 +0100

    EDA for airship done

---
+++ assignments/rzehulka/pso.py
@@ -0,0 +1,7 @@
+#def pso(swarm_size, obj_fct):
+#    x = np.random.randint((10,10))
+#    v = np.random.randint((10,10))
+#    iter_no = int(100/swarm_size)
+#    for i in range(iter_no):
+#        y = evaluate(x[i,:])
+#        v   
